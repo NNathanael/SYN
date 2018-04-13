104426ceeb112b41743b6530d7df9f4b329c5b0c
LW <loic.willy@gmail.com>
2018-04-13 19:07:22 +0200

    View website finish Data get from database

--- routes/orders.js
+++ routes/orders.js
@@ -1,24 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
 var mongoose = require('mongoose');
 var Restaurant = require('./../models/restaurant/restaurantSchema');
-
 
 /* GET restaurant orders. */
 router.get('/:name', function(req, res, next) {
 	mongoose.connect('mongodb://localhost/restaurant', function (err) {
-	if (err) throw err;
-	var nameResto = req.params.name;
+	    if (err) throw err;
+	    var nameResto = req.params.name;
 
-	Restaurant.find({ name: nameResto }, function(err, resto) {
-  if (err) throw err;
-  // object of the user
-  res.send(
-  	JSON.stringify(resto[0]));
-  });
-});
-	
+	    Restaurant.find({ name: nameResto }, function(err, resto) {
+            if (err) throw err;
+            res.render('orders', { restaurant: resto[0] });
+        });
+    });
 });
 
 module.exports = router;
+
